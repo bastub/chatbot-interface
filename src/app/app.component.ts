3cfeeaa1839614a9a0d591a1a24a6dd41f31066d
bastub <bastub57@gmail.com>
2024-06-04 22:15:42 +0200

    changed the bot icon and added recommandations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,26 +28,85 @@ export class AppComponent {
 
   constructor(private renderer: Renderer2, private el: ElementRef) {}
 
-  sendMessage() {
+  sendMessageInput() {
     const message = document.getElementById(
       'chat-input-box'
     ) as HTMLInputElement;
     const chat = document.getElementById('chat-messages');
 
-    if (message && chat) {
-      if (message.value === '') {
-        return;
-      }
+    if (message.value === '') {
+      return;
+    }
+
+    this.sendMessage(message.value);
+    message.value = '';
+  }
+
+  sendMessage(message: string) {
+    const chat = document.getElementById('chat-messages');
+
+    if (chat) {
       const newMessage_container = this.renderer.createElement('div');
       const newMessage_content = this.renderer.createElement('p');
       this.renderer.addClass(newMessage_container, 'chat-message-user');
       this.renderer.addClass(newMessage_content, 'chat-message-user-content');
-      const newMessage_text = this.renderer.createText(message.value);
+      const newMessage_text = this.renderer.createText(message);
       this.renderer.appendChild(newMessage_content, newMessage_text);
       this.renderer.appendChild(newMessage_container, newMessage_content);
       this.renderer.appendChild(chat, newMessage_container);
       chat.scrollTop = chat.scrollHeight;
-      message.value = '';
+    }
+
+    // randomly pick a number between 0 and 5
+    const random = Math.floor(Math.random() * 6);
+    let botMessage = '';
+    if (random === 0) {
+      botMessage = "Cherche sur Google stp j'existe pas encore";
+    } else if (random === 1) {
+      botMessage = 'Au pif je dirais 12';
+    } else if (random === 2) {
+      botMessage = 'Parle moi sur un autre ton.';
+    } else if (random === 3) {
+      botMessage = 'Aucune idée, question suivante';
+    } else if (random === 4) {
+      botMessage = "Lalala j'écoute pas";
+    } else if (random === 5) {
+      botMessage = message + ' CHIPS HAHAHAHAAA';
+    }
+
+    this.aiMessage(botMessage);
+  }
+
+  aiMessage(message: string) {
+    const chat = document.getElementById('chat-messages');
+
+    if (chat) {
+      const newMessage_container = this.renderer.createElement('div');
+      const newMessage_content = this.renderer.createElement('p');
+      const ai_photo_container = this.renderer.createElement('div');
+      const ai_photo = this.renderer.createElement('div');
+      const ai_photo_icon = this.renderer.createElement('img');
+      this.renderer.addClass(newMessage_container, 'chat-message-ai');
+      this.renderer.addClass(newMessage_content, 'chat-message-ai-content');
+      this.renderer.addClass(
+        ai_photo_container,
+        'chat-message-ai-photo-container'
+      );
+      this.renderer.addClass(ai_photo, 'chat-message-ai-photo');
+      this.renderer.setAttribute(
+        ai_photo_icon,
+        'src',
+        'assets/images/bot_png.png'
+      );
+      this.renderer.setAttribute(ai_photo_icon, 'width', '40px');
+      this.renderer.appendChild(ai_photo, ai_photo_icon);
+      this.renderer.appendChild(ai_photo_container, ai_photo);
+      this.renderer.appendChild(newMessage_container, ai_photo_container);
+      const newMessage_text = this.renderer.createText(message);
+      this.renderer.appendChild(newMessage_content, newMessage_text);
+      this.renderer.appendChild(newMessage_container, newMessage_content);
+      this.renderer.appendChild(chat, newMessage_container);
+      chat.scrollTop = chat.scrollHeight;
     }
   }
 }
