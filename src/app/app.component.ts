5a4f4a196519589ae77232a1ee1c8fa2ec65282f
bastub <bastub57@gmail.com>
2024-06-04 17:17:19 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Renderer2, ElementRef, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'chatbot-interface';
+
+  toggleChat() {
+    const chat = document.getElementById('chat-container');
+
+    // count the number of classes in the element chat
+
+    if (chat) {
+      const nb = chat.classList.length;
+      chat.classList.toggle('chat-open');
+      if (nb > 0) {
+        chat.classList.toggle('chat-closed');
+      }
+    }
+  }
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  sendMessage() {
+    const message = document.getElementById(
+      'chat-input-box'
+    ) as HTMLInputElement;
+    const chat = document.getElementById('chat-messages');
+
+    if (message && chat) {
+      if (message.value === '') {
+        return;
+      }
+      const newMessage_container = this.renderer.createElement('div');
+      const newMessage_content = this.renderer.createElement('p');
+      this.renderer.addClass(newMessage_container, 'chat-message-user');
+      this.renderer.addClass(newMessage_content, 'chat-message-user-content');
+      const newMessage_text = this.renderer.createText(message.value);
+      this.renderer.appendChild(newMessage_content, newMessage_text);
+      this.renderer.appendChild(newMessage_container, newMessage_content);
+      this.renderer.appendChild(chat, newMessage_container);
+      chat.scrollTop = chat.scrollHeight;
+      message.value = '';
+    }
+  }
 }
