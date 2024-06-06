24ea933fd00501d9805e2447d4909e2fbce0b762
bastub <bastub57@gmail.com>
2024-06-06 11:21:24 +0200

    added iframe, version and feedbacks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,10 @@ import { MatSlideToggle } from '@angular/material/slide-toggle';
 })
 export class AppComponent {
   title = 'chatbot-interface';
-  isHuman = false;
+  isHuman = true;
   showSettings = false;
   isBlueFilter = false;
+  isFeedback = false;
 
   toggleChat() {
     const chat = document.getElementById('chat-container');
@@ -101,13 +102,25 @@ export class AppComponent {
     const chat = document.getElementById('chat-messages');
 
     if (chat) {
-      const newMessage_container = this.renderer.createElement('div');
+      const newMessage_container_1 = this.renderer.createElement('div');
+      const newMessage_container_2 = this.renderer.createElement('div');
       const newMessage_content = this.renderer.createElement('p');
       const ai_photo_container = this.renderer.createElement('div');
       const ai_photo = this.renderer.createElement('div');
       const ai_photo_icon = this.renderer.createElement('img');
-      this.renderer.addClass(newMessage_container, 'chat-message');
-      this.renderer.addClass(newMessage_container, 'chat-message-ai');
+      const ai_thumbs = this.renderer.createElement('div');
+      const ai_thumb_up = this.renderer.createElement('span');
+      const ai_thumb_down = this.renderer.createElement('span');
+      this.renderer.addClass(newMessage_container_1, 'chat-message');
+      this.renderer.addClass(
+        newMessage_container_1,
+        'chat-message-ai-container'
+      );
+      this.renderer.addClass(newMessage_container_1, 'chat-message-ai');
+      this.renderer.addClass(
+        newMessage_container_2,
+        'chat-message-ai-content-container'
+      );
       this.renderer.addClass(newMessage_content, 'chat-message-ai-content');
       this.renderer.addClass(
         ai_photo_container,
@@ -121,13 +134,39 @@ export class AppComponent {
       );
       this.renderer.setAttribute(ai_photo_icon, 'width', '40px');
       this.renderer.setAttribute(ai_photo_icon, 'alt', 'Illustration de robot');
+      this.renderer.addClass(ai_thumbs, 'chat-message-ai-thumbs');
+      if (this.isFeedback) {
+        this.renderer.addClass(ai_thumbs, 'visible');
+      }
+      this.renderer.addClass(ai_thumb_up, 'material-symbols-outlined');
+      // this.renderer.addClass(ai_thumb_up, 'chat-message-ai-thumb');
+      this.renderer.addClass(ai_thumb_up, 'chat-message-ai-thumb-up');
+      this.renderer.addClass(ai_thumb_down, 'material-symbols-outlined');
+      // this.renderer.addClass(ai_thumb_up, 'chat-message-ai-thumb');
+      this.renderer.addClass(ai_thumb_down, 'chat-message-ai-thumb-down');
+      ai_thumb_up.innerHTML = 'thumb_up';
+      ai_thumb_down.innerHTML = 'thumb_down';
+
+      // Ajoute des écouteurs d'événements
+      this.renderer.listen(ai_thumb_up, 'click', (event) =>
+        this.thumbUp(event.target)
+      );
+      this.renderer.listen(ai_thumb_down, 'click', (event) =>
+        this.thumbDown(event.target)
+      );
+
+      this.renderer.appendChild(ai_thumbs, ai_thumb_up);
+      this.renderer.appendChild(ai_thumbs, ai_thumb_down);
       this.renderer.appendChild(ai_photo, ai_photo_icon);
       this.renderer.appendChild(ai_photo_container, ai_photo);
-      this.renderer.appendChild(newMessage_container, ai_photo_container);
+      this.renderer.appendChild(newMessage_container_1, ai_photo_container);
       const newMessage_text = this.renderer.createText(message);
       this.renderer.appendChild(newMessage_content, newMessage_text);
-      this.renderer.appendChild(newMessage_container, newMessage_content);
-      this.renderer.appendChild(chat, newMessage_container);
+      this.renderer.appendChild(newMessage_container_2, newMessage_content);
+      this.renderer.appendChild(newMessage_container_2, ai_thumbs);
+      this.renderer.appendChild(newMessage_container_1, newMessage_container_2);
+
+      this.renderer.appendChild(chat, newMessage_container_1);
       chat.scrollTop = chat.scrollHeight;
     }
   }
@@ -137,5 +176,39 @@ export class AppComponent {
     while (messages.length > 0) {
       messages[0].remove();
     }
+  }
+
+  toggleFeedback() {
+    this.isFeedback = !this.isFeedback;
+    // get all the elements with class thumbs
+    const thumbs = document.getElementsByClassName('chat-message-ai-thumbs');
+    for (let i = 0; i < thumbs.length; i++) {
+      if (this.isFeedback) {
+        thumbs[i].classList.add('visible');
+      }
+      if (!this.isFeedback) {
+        thumbs[i].classList.remove('visible');
+      }
+    }
+  }
+
+  thumbUp(target: any) {
+    console.log('thumb up');
+    target.classList.toggle('material-symbols-filled');
+    target.classList.toggle('chat-message-ai-thumb-up-clicked');
+    target.nextElementSibling.classList.remove('material-symbols-filled');
+    target.nextElementSibling.classList.remove(
+      'chat-message-ai-thumb-down-clicked'
+    );
+  }
+
+  thumbDown(target: any) {
+    console.log('thumb down');
+    target.classList.toggle('material-symbols-filled');
+    target.classList.toggle('chat-message-ai-thumb-down-clicked');
+    target.previousElementSibling.classList.remove('material-symbols-filled');
+    target.previousElementSibling.classList.remove(
+      'chat-message-ai-thumb-up-clicked'
+    );
   }
 }
