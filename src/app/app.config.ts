8821113d68974e8b501777eca0b73a3e35e92df5
Imadm12 <118177419+Imadm12@users.noreply.github.com>
2024-06-12 12:45:40 +0200

    Update app.config.ts
    Added HTTP Client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { provideHttpClient, withFetch } from '@angular/common/http'; // Importez withFetch
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -14,6 +15,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideAnimationsAsync(),
+    provideHttpClient(withFetch()), 
     {
       provide: RECAPTCHA_SETTINGS,
       useValue: {
